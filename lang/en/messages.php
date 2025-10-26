83e416965dcfc1715533030450979144d0f61227
Mateusz <mkordys98@gmail.com>
2025-10-26 20:19:49 +0100

    ENG version support for Landing

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -21,7 +21,7 @@ return [
         'lead' => 'A fast, lightweight note-taking app with secure synchronization.',
         'cta_android' => 'Download for Android',
         'cta_ios' => 'Download for iOS',
-        'cta_ios_tooltip' => 'iOS version temporarily unavailable. </br>We are working on the App Store release.',
+        'cta_ios_tooltip' => 'iOS version temporarily unavailable.  We are working on the App Store release.',
         'cta_expo' => 'Preview in Expo GO',
     ],
     'shots' => [
