7b3452db9ab65a4649fcd975c1c2540ffaaab721
Mateusz <mkordys98@gmail.com>
2025-10-26 18:57:40 +0100

    Landing page Light/Dark mode synchronised with Mobile app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1" />
-    <title>NoteSync — Wspólne notatki to lepsza współpraca.</title>
+    <title>NoteSync — Wspólne notatki to lepsza współpraca</title>
 
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&family=Pacifico&display=swap" rel="stylesheet" />
 
